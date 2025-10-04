297d40af48c6e735ad1e1faa71a7f5dcfd4b2dc5
bossk538 <santiago538@yahoo.com>
2025-10-04 12:56:32 -0400

    feat: adding 2048 route

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Foo } from './Foo';
+import { TFE } from './2048/2048';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/foo" element={<Foo />} />
+        <Route path="/2048" element={<TFE />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
