d17a59c9d99e5a6d20960aa3cf3c9e8ae4259090
bossk538 <santiago538@yahoo.com>
2025-11-14 12:09:52 -0500

    feat: adding links to projects to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -24,6 +25,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+	<ul className="link-list">
+          <li><NavLink to="/2048">2048</NavLink></li>
+          <li><NavLink to="/canvas">Canvas</NavLink></li>
+          <li><NavLink to="/svg">SVG</NavLink></li>
+          <li><NavLink to="/liquid-glass">Liquid Glass</NavLink></li>
+	</ul>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
