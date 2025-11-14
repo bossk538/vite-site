0d995e0b4512b2b9212d876c8eefbbde6dea7b26
bossk538 <santiago538@yahoo.com>
2025-11-14 15:03:56 -0500

    adding D3 to svg app

--- src/svg/index.tsx
+++ src/svg/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
 export const SvgApp = () => {
-    return <h2>SVG App</h2>;
+    const svgRef = useRef();
+
+    useEffect(() => {
+	    const svgElement = d3.select(svgRef.current);
+	    svgElement.append('circle')
+	        .attr('cx', 150)
+		.attr('cy', 70)
+		.attr('r', 50);
+    }, []);
+
+    return (
+	<div>
+	    <h2>SVG App</h2>
+	    <svg ref={svgRef} />
+	</div>
+    );
 };
