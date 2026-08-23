4f652b7991af30b11f0e2adb6940bab2bc76a80a
bossk538 <santiago538@yahoo.com>
2026-08-23 18:57:18 -0400

    feat: adding Tic-Tac-Toe II game

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { TFE } from './2048/2048';
 import { LGR } from './liquid-glass';
 import { CanvasApp } from './canvas';
 import { SvgApp } from './svg';
+import { TicTacToe2 } from './tic-tac-toe2';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/canvas" element={<CanvasApp />} />
         <Route path="/svg" element={<SvgApp />} />
         <Route path="/liquid-glass" element={<LGR />} />
+        <Route path="/tic-tac-toe2" element={<TicTacToe2 />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
