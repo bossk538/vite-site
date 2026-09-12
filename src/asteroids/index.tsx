b22fc90ac59a7897ca54fe00dc2bf6065919d809
bossk538 <santiago538@yahoo.com>
2026-09-12 15:15:44 -0400

    feat: factor out Asteroid to own component

--- src/asteroids/index.tsx
+++ src/asteroids/index.tsx
@@ -8,12 +8,11 @@ const createOutline = () => {
   console.log(ary);
   for (const degrees of ary) {
     const radians = degrees * Math.PI / 180;
-    const radius = Math.random() * 100 + 10;
+    const radius = Math.random() * 50 + 50;
     coords.push(Math.trunc(radius * Math.cos(radians)));
     coords.push(Math.trunc(radius * Math.sin(radians)));
   }
-  //const path = `M ${coords[0]} ${coords[1]} ` +  coords.join(' ') + ' Z';
-  return coords.join(' ')// + ' Z';;
+  return coords.join(' ');
 };
 
 const wrap = (value, max) => {
@@ -26,27 +25,48 @@ const wrap = (value, max) => {
   }
 };
 
+const Asteroid = ({ x, y, path, angle }) => {
+  return ( 
+    <g transform={`translate(${x} ${y})`}>
+      <polygon points={`${path}`} stroke="black" fill="none"  transform={`rotate(${angle})`} />
+    </g>
+  );
+};
+
+const createAsteroid = (width, height) => {
+  return {
+    degreesPerSecond: 50 * (Math.random()- .5),
+    angle: Math.random() * 360,
+    x: Math.random() * width,
+    y: Math.random() * height,
+    deltaX: 5 * (Math.random() - .5),
+    deltaY: 5 * (Math.random() - .5),
+    path: createOutline(),
+  };
+};
+
+const updateAsteroid = (asteroid, deltaSeconds, width, height) => {
+    const { x, y, deltaX, deltaY, angle, degreesPerSecond } = asteroid;
+    return {
+      ...asteroid,
+      x: wrap(x + deltaX, width),
+      y: wrap(y + deltaY, height),
+      angle: (angle + degreesPerSecond * deltaSeconds) % 360,
+    };
+};
+
 export default function Asteroids({ size = 1000, color = 'steelblue' }) {
   const width = 1000;
   const height = 500;
-  const [angle, setAngle] = useState(0);
   const lastTimeRef = useRef(null);
   const frameRef = useRef(null);
-  const fooRef = useRef(null);
-  const [path, setPath] = useState(() => createOutline());
-  const [degreesPerSecond, setDegreesPerSecond] = useState(() => (50 * (Math.random()- .5)));
-  const [deltaX, setDeltaX] = useState(() => 5 * (Math.random() - .5))
-  const [deltaY, setDeltaY] = useState(() => 5 * (Math.random() - .5))
-  const [x, setX] = useState<number>(0);
-  const [y, setY] = useState<number>(0);
+  const [asteroid, setAsteroid] = useState(() => createAsteroid(width, height));
 
   useEffect(() => {
     const animate = (time) => {
       if (lastTimeRef.current !== null) {
         const deltaSeconds = (time - lastTimeRef.current) / 1000;
-        setAngle((prev) => (prev + degreesPerSecond * deltaSeconds) % 360);
-        setX(prev => wrap(prev + deltaX, width));
-        setY(prev => wrap(prev + deltaY, height));
+        setAsteroid(prev => updateAsteroid(prev, deltaSeconds, width, height));
       }
       lastTimeRef.current = time;
       frameRef.current = requestAnimationFrame(animate);
@@ -55,18 +75,15 @@ export default function Asteroids({ size = 1000, color = 'steelblue' }) {
     frameRef.current = requestAnimationFrame(animate);
 
     return () => cancelAnimationFrame(frameRef.current);
-  }, [degreesPerSecond]);
+  }, []);
 
   const viewSize = size * 1.5; // extra space so rotation doesn't clip
   const center = viewSize / 2;
   const half = size / 2;
-  //const path = `M10 10 90 10 90 90`;
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
-    <g transform={`translate(${x} ${y})`}>
-      <polygon ref={fooRef} points={`${path}`} stroke="black" fill="none"  transform={`rotate(${angle} ${x} ${y})`} />
-      </g>
+      <Asteroid x={asteroid.x} y={asteroid.y} path={asteroid.path} angle={asteroid.angle} />
     </svg>
   );
 }
